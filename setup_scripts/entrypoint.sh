5fd3909c37810b885db91526c8fa1c9619332e7a
putm <Adam_Wasilewski_99@proton.me>
2023-03-14 19:34:44 +0100

    Restructure code to start a clean catkin_ws

--- setup_scripts/entrypoint.sh
+++ setup_scripts/entrypoint.sh
@@ -10,11 +10,13 @@ fi
 
 
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-cd /home/putm/catkin_ws
 source /opt/ros/noetic/setup.bash
+cd /home/putm/catkin_ws/
 rm -r build/
 rm -r devel/
 catkin_make
+rm -r src/
 mkdir src/
 echo "source /home/putm/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
 roscore
