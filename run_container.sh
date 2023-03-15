a74078f909e1ec842b856897ba247541eaa28d83
KozAAAAA <mateusz-koza@outlook.com>
2023-03-15 10:00:54 +0100

    Add automatic path include

--- run_container.sh
+++ run_container.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-set -e
 
-PATH_TO_SHARE_WITH_CONTAINER="/home/mateusz/PUTM_DV_MAIN_CATKIN_WORKSPACE/catkin_ws/"
+PATH_TO_SHARE_WITH_CONTAINER="$PWD/catkin_ws/"
+#PATH_TO_SHARE_WITH_CONTAINER="<your_path_to_this_folder>/PUTM_DV_MAIN_CATKIN_WORKSPACE/catkin_ws/"
 
 IMAGE_NAME="putm_dv_main_catkin_workspace"
+#IMAGE_NAME="<your_image_name>"
+
 CONTAINER_NAME="putm_dv_main_catkin_workspace"
+#CONTAINER_NAME="your_container_name"
+
+
+set -e
 
 docker build -t $IMAGE_NAME ./docker/
 
