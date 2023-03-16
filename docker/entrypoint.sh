0077cdc468b7ee389eb813d934c6fb94bc317377
KozAAAAA <mateusz-koza@outlook.com>
2023-03-16 14:59:20 +0100

    Add CONTAINER READY message when running the container, change gitignore to keep the src folder

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,19 +3,17 @@
 echo "source /home/putm/catkin_ws/devel/setup.bash" >> ~/.bashrc
 source /opt/ros/noetic/setup.bash
 
-
 ./setup_can.sh
 ./setup_joy.sh
-sleep 5
+sleep 3
 # if [ $? -ne 0 ]; then
 #   exit 1
 # fi
 
-
 cd /home/putm/catkin_ws/
-rm -r *
-mkdir src/
-touch .gitkeep
+
 catkin_make
+
+echo -e "\033[0;32m---> CONTAINER IS READY <---\033[0m"
 
 roscore
