4c0cc50585032240da1c51e40807bc04a7069366
putm <Adam_Wasilewski_99@proton.me>
2023-03-17 16:24:08 +0100

    Add xsens dependencies

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "source /home/putm/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo 'source /home/putm/catkin_ws/devel/setup.bash' >> ~/.bashrc
+echo 'source /opt/ros/noetic/setup.bash' >> ~/.bashrc
+
 source /opt/ros/noetic/setup.bash
 
 ./setup_can.sh
