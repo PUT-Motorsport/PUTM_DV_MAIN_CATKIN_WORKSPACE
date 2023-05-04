a15349cde806ba0a58d251a3eb8cb432651299e3
putm <Adam_Wasilewski_99@proton.me>
2023-05-04 19:41:13 +0200

    Add automatic bag recording on startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo '/opt/ros/noetic/setup.bash' >> ~/.bashrc
+
+################## DO NOT TOUCH ##################
+echo 'source /opt/ros/noetic/setup.bash' >> ~/.bashrc
 echo 'source /home/putm/catkin_ws/devel/setup.bash' >> ~/.bashrc
 ./setup_can.sh
 ./setup_joy.sh
 ./detect_xsens.sh
-sleep 3
+sleep 2
 # if [ $? -ne 0 ]; then
 #   exit 1
 # fi
@@ -12,12 +14,20 @@ source /opt/ros/noetic/setup.bash
 cd /home/putm/catkin_ws/
 catkin_make
 source devel/setup.bash
-
-############## ADD ROSLAUNCH BELOW ###############
-roslaunch xsens_mti_driver xsens_mti_node.launch &
-
-
+roscore &
+sleep 2
 ##################################################
 
+
+
+############### ADD ROSLAUNCH HERE ###############
+roslaunch xsens_mti_driver xsens_mti_node.launch &
+rosbag record -a --split --duration=2m &
+##################################################
+
+
+
+################## DO NOT TOUCH ##################
 echo -e "\033[0;32m---> CONTAINER IS READY <---\033[0m"
 sleep infinity
+##################################################
