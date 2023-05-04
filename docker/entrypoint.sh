8ce47b8170d49b1b7deb681cd56cafc0ffbebd80
putm <Adam_Wasilewski_99@proton.me>
2023-05-04 18:55:00 +0200

    Run roslaunch files in entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
+echo '/opt/ros/noetic/setup.bash' >> ~/.bashrc
 echo 'source /home/putm/catkin_ws/devel/setup.bash' >> ~/.bashrc
-source /opt/ros/noetic/setup.bash
-
 ./setup_can.sh
 ./setup_joy.sh
 ./detect_xsens.sh
@@ -10,11 +8,16 @@ sleep 3
 # if [ $? -ne 0 ]; then
 #   exit 1
 # fi
-
+source /opt/ros/noetic/setup.bash
 cd /home/putm/catkin_ws/
-
 catkin_make
+source devel/setup.bash
+
+############## ADD ROSLAUNCH BELOW ###############
+roslaunch xsens_mti_driver xsens_mti_node.launch &
+
+
+##################################################
 
 echo -e "\033[0;32m---> CONTAINER IS READY <---\033[0m"
-
-roscore
+sleep infinity
