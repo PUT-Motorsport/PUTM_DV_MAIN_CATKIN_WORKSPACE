fcb44735b98307e31d34bc73e694b60cf66b0073
putm <Adam_Wasilewski_99@proton.me>
2023-05-05 21:47:13 +0200

    Move seperate solaunch files into one

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,20 +14,7 @@ source /opt/ros/noetic/setup.bash
 cd /home/putm/catkin_ws/
 catkin_make
 source devel/setup.bash
-roscore &
-sleep 2
-##################################################
 
-
-
-############### ADD ROSLAUNCH HERE ###############
-roslaunch xsens_mti_driver xsens_mti_node.launch &
-rosbag record -a --split --duration=2m &
-##################################################
-
-
-
-################## DO NOT TOUCH ##################
 echo -e "\033[0;32m---> CONTAINER IS READY <---\033[0m"
-sleep infinity
+roslaunch launch/main.launch
 ##################################################
